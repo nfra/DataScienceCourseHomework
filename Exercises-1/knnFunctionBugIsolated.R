cd9cfd36ff719af365a217f9641a98e38915e003
Nathan Franz <37715299+nfra@users.noreply.github.com>
2020-02-03 15:50:12 -0600

    Fixed error in documentation, removed unnecessary step

--- Exercises-1/knnFunctionBugIsolated.R
+++ Exercises-1/knnFunctionBugIsolated.R
@@ -18,16 +18,13 @@ train_indices = sample.int(N, N_train, replace=FALSE)
 D_train = sclass[train_indices,]
 D_test = sclass[-train_indices,]
 
-# Reorder the rows of the testing set by the "mileage" variable
-D_test = arrange(D_test, mileage)
-
 # Separate training and testing sets into features (X) and outcome (y)
 X_train = select(D_train, mileage)
 y_train = select(D_train, price)
 x_test = select(D_test, mileage)
 y_test = select(D_test, price)
 
-# Fit KNN for each k = 2
+# Fit KNN for k = 2
 # NOTE: KNN bugs out for k-value 2
 knn2 = knn.reg(train = X_train, test = x_test, y = y_train, k=2)
 
