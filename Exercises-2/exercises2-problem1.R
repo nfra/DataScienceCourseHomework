38e47cc01b524d117544d813d4820be20dc37c1a
Nathan <37715299+nfra@users.noreply.github.com>
2020-02-23 13:09:41 -0600

    improved model and added initial KNN reg
    Still need to make numeric (I think?) and scale by sd for distance calculation

--- Exercises-2/exercises2-problem1.R
+++ Exercises-2/exercises2-problem1.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(mosaic)
+library(FNN)
 
 data(SaratogaHouses)
 
@@ -30,17 +31,36 @@ baseline_rmse = rmse(saratoga_test$price, yhat_test_baseline)
 
 
 
-# Calculate RMSE for better model
+# Calculate RMSE for better linear model
 
 lm_better = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + 
                  fireplaces + bathrooms + rooms + heating + fuel + centralAir +
                  heating*(fireplaces + fuel) + 
                  bedrooms*(rooms + bathrooms) +
                  livingArea*(rooms + lotSize) +
-                 lotSize*pctCollege + 
+                 lotSize*(pctCollege + rooms) +
                  age*(lotSize + livingArea + bedrooms + bathrooms + heating + centralAir), 
                data=saratoga_train)
 
 yhat_test_better = predict(lm_better, saratoga_test)
 
 better_rmse = rmse(saratoga_test$price, yhat_test_better)
+
+# Make data numeric and scale by standard deviation for KNN
+
+
+
+# Calculate RMSE for KNN
+
+saratoga_train_x = saratoga_train[,c("lotSize", "age", "livingArea", "pctCollege", 
+                                     "bedrooms", "fireplaces", "bathrooms", 
+                                     "rooms", "heating", "fuel", "centralAir")]
+saratoga_train_y = saratoga_train$price
+saratoga_test_x = saratoga_test[,c("lotSize", "age", "livingArea", "pctCollege", 
+                                   "bedrooms", "fireplaces", "bathrooms", 
+                                   "rooms", "heating", "fuel", "centralAir")]
+saratoga_test_y = saratoga_test$price
+
+knn_better = knn.reg(train=saratoga_train_x, test=saratoga_test_x, 
+                     y=saratoga_train_y, k=5)
+
