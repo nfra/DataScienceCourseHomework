ded6425518a479cd22d4b95cb3fe95f069823051
Nathan <37715299+nfra@users.noreply.github.com>
2020-03-04 17:50:02 -0600

    added plot of radiologist diffs with 95%CI

--- Exercises-2/scripts/exercises2-problem2.R
+++ Exercises-2/scripts/exercises2-problem2.R
@@ -21,11 +21,20 @@ confusion_matrix_radiologist89
 # Plot c
 
 radiologist_estimates = data.frame(summary(binomial_recall_model)$coefficients[2:5,])
-radiologist_comparison_data = data.frame()
-radiologist_comparison_data$ci_low = radiologist_estimates[,1] + 1.96*radiologist_comparison_data[,2]
+radiologist_estimates$radiologist = c('34', '66', '89', '95')
+radiologist_estimates$ci_low = radiologist_estimates[,1] - 1.96*radiologist_comparison_data[,2]
+radiologist_estimates$ci_high = radiologist_estimates[,1] + 1.96*radiologist_comparison_data[,2]
+radiologist_estimates$est_lkhd = exp(radiologist_estimates$Estimate)
+radiologist_estimates$ci_low_lkhd = exp(radiologist_estimates$ci_low)
+radiologist_estimates$ci_high_lkhd = exp(radiologist_estimates$ci_high)
 
-ggplot(data = radiologist_comparison_data) +
-  geom_pointrange()
+
+
+ggplot(aes(x = radiologist,
+           y = Estimate), 
+       data = radiologist_estimates) +
+  geom_pointrange(aes(ymin = ci_low, ymax = ci_high)) +
+  geom_hline(yintercept = 0, color = "red")
 
 # Part 2
 
